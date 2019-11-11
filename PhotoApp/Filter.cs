cc5fb82bae068385d21cba12e17d3bdc819eb9f1
DongNguyen20 <48273417+DongNguyen20@users.noreply.github.com>
2019-11-11 22:05:38 +0700

    Update Filter.cs

--- PhotoApp/Filter.cs
+++ PhotoApp/Filter.cs
@@ -1,12 +1,178 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace PhotoApp
 {
-    class Filter
+    public class ConvMatrix
     {
+        public int TopLeft = 0, TopMid = 0, TopRight = 0;
+        public int MidLeft = 0, Pixel = 1, MidRight = 0;
+        public int BottomLeft = 0, BottomMid = 0, BottomRight = 0;
+        public int Factor = 1;
+        public int Offset = 0;
+        public void SetAll(int nVal)
+        {
+            TopLeft = TopMid = TopRight = MidLeft = Pixel = MidRight = BottomLeft = BottomMid = BottomRight = nVal;
+        }
     }
+    public struct FloatPoint
+    {
+        public double X;
+        public double Y;
+    }
+
+    public class BitmapFilter
+    {
+        public const short EDGE_DETECT_KIRSH   = 1;
+        public const short EDGE_DETECT_PREWITT = 2;
+        public const short EDGE_DETECT_SOBEL   = 3;
+
+        public static bool GrayScale(Bitmap b)
+        {
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                int nOffset = stride - b.Width * 3;
+                byte red, green, blue;
+                for (int y = 0; y < b.Height; y++)
+                {
+                    for (int x = 0; x < b.Width; x++)
+                    {
+                        blue = p[0];
+                        green = p[1];
+                        red = p[2];
+                        p[0] = p[1] = p[2] = (byte)(.299 * red + .587 * green + .114 * blue);
+                        p += 3;
+
+                    }
+                    p += nOffset;
+                }
+            }
+            
+
+            b.UnlockBits(bmData);
+            return true;
+       
+        }
+
+        public static bool Brightness(Bitmap b, int nBright)
+        {
+            if (nBright < -255 || nBright > 255)
+                return false;
+
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+            int nVal = 0;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+                int nOffset = stride - b.Width * 3;
+                int nWidth = b.Width * 3;
+
+                for(int  y=0; y<b.Height; y++)
+                {
+                    for(int x =0; x < nWidth; x++)
+                    {
+                        nVal = (int)(p[0] + nBright);
+                        if(nVal < 0) nVal = 0;
+                        if (nVal > 255) nVal = 255;
+
+                        p[0] = (byte)nVal;
+                        ++p;
+                    }
+                    p += nOffset;
+                }
+            }
+            b.UnlockBits(bmData);
+            return true;
+        }
+
+        public static bool Invert(Bitmap b)
+        {
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+
+                int nOffset = stride - b.Width * 3;
+                int nWidth = b.Width * 3;
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < nWidth; ++x)
+                    {
+                        p[0] = (byte)(255 - p[0]);
+                        ++p;
+                    }
+                    p += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+
+            return true;
+        }
+
+        public static bool Color(Bitmap b, int red, int green, int blue)
+        {
+            if (red < -255 || red > 255) return false;
+            if (green < -255 || green > 255) return false;
+            if (blue < -255 || blue > 255) return false;
+
+            // GDI+ still lies to us - the return format is BGR, NOT RGB.
+            BitmapData bmData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+
+            int stride = bmData.Stride;
+            System.IntPtr Scan0 = bmData.Scan0;
+
+            unsafe
+            {
+                byte* p = (byte*)(void*)Scan0;
+
+                int nOffset = stride - b.Width * 3;
+                int nPixel;
+
+                for (int y = 0; y < b.Height; ++y)
+                {
+                    for (int x = 0; x < b.Width; ++x)
+                    {
+                        nPixel = p[2] + red;
+                        nPixel = Math.Max(nPixel, 0);
+                        p[2] = (byte)Math.Min(255, nPixel);
+
+                        nPixel = p[1] + green;
+                        nPixel = Math.Max(nPixel, 0);
+                        p[1] = (byte)Math.Min(255, nPixel);
+
+                        nPixel = p[0] + blue;
+                        nPixel = Math.Max(nPixel, 0);
+                        p[0] = (byte)Math.Min(255, nPixel);
+
+                        p += 3;
+                    }
+                    p += nOffset;
+                }
+            }
+
+            b.UnlockBits(bmData);
+
+            return true;
+        }
+    }
+
 }
