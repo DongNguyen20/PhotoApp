2480251ea04cd4033973f3136e9c2db20e269df7
DongNguyen20 <48273417+DongNguyen20@users.noreply.github.com>
2019-11-11 19:31:18 +0700

    Add files via upload

---
+++ PhotoApp/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace PhotoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.file = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoadFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.filter = new System.Windows.Forms.ToolStripMenuItem();
+            this.gray = new System.Windows.Forms.ToolStripMenuItem();
+            this.bright = new System.Windows.Forms.ToolStripMenuItem();
+            this.invert = new System.Windows.Forms.ToolStripMenuItem();
+            this.color = new System.Windows.Forms.ToolStripMenuItem();
+            this.edit = new System.Windows.Forms.ToolStripMenuItem();
+            this.undo = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom25 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom50 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom100 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom150 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom200 = new System.Windows.Forms.ToolStripMenuItem();
+            this.zoom300 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.file,
+            this.filter,
+            this.edit,
+            this.zoom});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(618, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // file
+            // 
+            this.file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.LoadFile,
+            this.saveFile,
+            this.exit});
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(37, 20);
+            this.file.Text = "File";
+            // 
+            // LoadFile
+            // 
+            this.LoadFile.Name = "LoadFile";
+            this.LoadFile.Size = new System.Drawing.Size(152, 22);
+            this.LoadFile.Text = "Load";
+            this.LoadFile.Click += new System.EventHandler(this.LoadFile_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(152, 22);
+            this.saveFile.Text = "Save";
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(152, 22);
+            this.exit.Text = "Exit";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // filter
+            // 
+            this.filter.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gray,
+            this.bright,
+            this.invert,
+            this.color});
+            this.filter.Name = "filter";
+            this.filter.Size = new System.Drawing.Size(45, 20);
+            this.filter.Text = "Filter";
+            // 
+            // gray
+            // 
+            this.gray.Name = "gray";
+            this.gray.Size = new System.Drawing.Size(129, 22);
+            this.gray.Text = "GrayScale";
+            // 
+            // bright
+            // 
+            this.bright.Name = "bright";
+            this.bright.Size = new System.Drawing.Size(129, 22);
+            this.bright.Text = "Brightness";
+            // 
+            // invert
+            // 
+            this.invert.Name = "invert";
+            this.invert.Size = new System.Drawing.Size(129, 22);
+            this.invert.Text = "Invert";
+            // 
+            // color
+            // 
+            this.color.Name = "color";
+            this.color.Size = new System.Drawing.Size(129, 22);
+            this.color.Text = "Color";
+            // 
+            // edit
+            // 
+            this.edit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undo});
+            this.edit.Name = "edit";
+            this.edit.Size = new System.Drawing.Size(39, 20);
+            this.edit.Text = "Edit";
+            // 
+            // undo
+            // 
+            this.undo.Name = "undo";
+            this.undo.Size = new System.Drawing.Size(103, 22);
+            this.undo.Text = "Undo";
+            // 
+            // zoom
+            // 
+            this.zoom.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zoom25,
+            this.zoom50,
+            this.zoom100,
+            this.zoom150,
+            this.zoom200,
+            this.zoom300});
+            this.zoom.Name = "zoom";
+            this.zoom.Size = new System.Drawing.Size(51, 20);
+            this.zoom.Text = "Zoom";
+            // 
+            // zoom25
+            // 
+            this.zoom25.Name = "zoom25";
+            this.zoom25.Size = new System.Drawing.Size(102, 22);
+            this.zoom25.Text = "25%";
+            // 
+            // zoom50
+            // 
+            this.zoom50.Name = "zoom50";
+            this.zoom50.Size = new System.Drawing.Size(102, 22);
+            this.zoom50.Text = "50%";
+            // 
+            // zoom100
+            // 
+            this.zoom100.Name = "zoom100";
+            this.zoom100.Size = new System.Drawing.Size(102, 22);
+            this.zoom100.Text = "100%";
+            // 
+            // zoom150
+            // 
+            this.zoom150.Name = "zoom150";
+            this.zoom150.Size = new System.Drawing.Size(102, 22);
+            this.zoom150.Text = "150%";
+            // 
+            // zoom200
+            // 
+            this.zoom200.Name = "zoom200";
+            this.zoom200.Size = new System.Drawing.Size(102, 22);
+            this.zoom200.Text = "200%";
+            // 
+            // zoom300
+            // 
+            this.zoom300.Name = "zoom300";
+            this.zoom300.Size = new System.Drawing.Size(102, 22);
+            this.zoom300.Text = "300%";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(618, 364);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Photo";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem file;
+        private System.Windows.Forms.ToolStripMenuItem LoadFile;
+        private System.Windows.Forms.ToolStripMenuItem saveFile;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+        private System.Windows.Forms.ToolStripMenuItem filter;
+        private System.Windows.Forms.ToolStripMenuItem gray;
+        private System.Windows.Forms.ToolStripMenuItem bright;
+        private System.Windows.Forms.ToolStripMenuItem invert;
+        private System.Windows.Forms.ToolStripMenuItem color;
+        private System.Windows.Forms.ToolStripMenuItem edit;
+        private System.Windows.Forms.ToolStripMenuItem undo;
+        private System.Windows.Forms.ToolStripMenuItem zoom;
+        private System.Windows.Forms.ToolStripMenuItem zoom25;
+        private System.Windows.Forms.ToolStripMenuItem zoom50;
+        private System.Windows.Forms.ToolStripMenuItem zoom100;
+        private System.Windows.Forms.ToolStripMenuItem zoom150;
+        private System.Windows.Forms.ToolStripMenuItem zoom200;
+        private System.Windows.Forms.ToolStripMenuItem zoom300;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ PhotoApp/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhotoApp
+{
+    public partial class Form1 : Form
+    {
+        private System.Drawing.Bitmap m_Bitmap;
+        private System.Drawing.Bitmap m_Undo;
+        private double Zoom = 1.0;
+        public Form1()
+        {
+            InitializeComponent();
+            m_Bitmap = new Bitmap(2, 2);
+        }
+        //Hien thi anh
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(m_Bitmap, new Rectangle(this.AutoScrollPosition.X, this.AutoScrollPosition.Y, (int)(m_Bitmap.Width * Zoom), (int)(m_Bitmap.Height * Zoom)));
+        }
+        // Mo file 
+        private void LoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.InitialDirectory = "c:\\";
+            openFile.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp/*.jpg";
+            openFile.FilterIndex = 2;
+            openFile.RestoreDirectory = true;
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                m_Bitmap = (Bitmap)Bitmap.FromFile(openFile.FileName, false);
+                this.AutoScroll = true;
+                this.AutoScrollMinSize = new Size((int)(m_Bitmap.Width * Zoom), (int)(m_Bitmap.Height * Zoom));
+                this.Invalidate();
+            }
+        }
+
+        //Luu file
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.InitialDirectory = "c:\\";
+            saveFile.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|All valid files (*.bmp/*.jpg)|*.bmp/*.jpg";
+            saveFile.FilterIndex = 1;
+            saveFile.RestoreDirectory = true;
+
+            if (DialogResult.OK == saveFile.ShowDialog())
+            {
+                m_Bitmap.Save(saveFile.FileName);
+            }
+        }
+
+        //Exit
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+    }
+}
